423d3cf8dee0b00947a6e608f38fc534d76a605b
“Ebibeni” <ebibeni.dbig@gmail.com>
2023-11-12 19:36:17 +0100

    Using the range function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 
+# count number 1 to 100 and look for a multiple of 3 , 5. thats what 5 & 3 can divide to get 0
 
-num = int(input("Enter a number: "))
-
-if num%2 == 0:
-    print("That is an even number")
-else:
-    print("That is an odd number")
+for x in range(1,101):
+    if x % 3 == 0 and x % 5 == 0:
+        print(x, " is a multiple of both 3 and 5")
+    elif x % 3 == 0:
+        print(x, " is a multiple of 3")
+    elif x % 5 == 0:
+        print(x, " is a multiple of 5")
+    else:
+        print(x)
