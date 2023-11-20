0008911acda60a8ce3c6b2fe2f175e8443292925
“Ebibeni” <ebibeni.dbig@gmail.com>
2023-11-20 08:57:55 +0100

    user_input

---
+++ userinput.py
@@ -0,0 +1,10 @@
+
+# How to get user input and use the if conditional statement
+
+n = int(input('Please enter a number: '))
+
+if n > 10:
+    print('That is a good number')
+
+else:
+    print('That is not a good number')
