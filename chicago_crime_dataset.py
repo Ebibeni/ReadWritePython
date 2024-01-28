6e8df0f34ede9cbd2e462e80ae8f3bd737b07f05
“Ebibeni” <ebibeni.dbig@gmail.com>
2024-01-28 20:47:59 +0100

    Adding New instruction set

--- chicago_crime_dataset.py
+++ chicago_crime_dataset.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+# Instruction
+# Get the data from the API first and then save it as a CSV file or a Json file
+# and then use the data from the file.
+
 #ID to UPDATED ON FIELDS ONLY.
 # Importing data via APi and assigning the request to Breweries_data
 url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json?"
@@ -14,7 +18,22 @@ def request_call(url_value):
 
 def data_manipulation(chicago_crime_data_json_value):
     length_chicago_crime_data = len(chicago_crime_data_json_value)
+    new_chicago_crime_data = {}
+
+    for index in range(length_chicago_crime_data):
+
+        # print(index)
+        for key in chicago_crime_data_json_value[index]:
+            # print(chicago_crime_data_json_value[index][key])
+            print(key)
+
+
     
 
-# chicago_crime_data_json_value = request_call(url)
+    
+
+
+chicago_crime_data_json_value = request_call(url)
 # print(data_manipulation(chicago_crime_data_json_value))
+# data_manipulation(chicago_crime_data_json_value)
+print(data_manipulation(chicago_crime_data_json_value))
