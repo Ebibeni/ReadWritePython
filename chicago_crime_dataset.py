a040da99fb2d39d602f1b243f9c61d967d6ae60f
“Ebibeni” <ebibeni.dbig@gmail.com>
2024-01-28 15:51:08 +0100

    request call function was added to the code

--- chicago_crime_dataset.py
+++ chicago_crime_dataset.py
@@ -1,7 +1,17 @@
 import requests
 import json
 
-
+#ID to UPDATED ON FIELDS ONLY.
 # Importing data via APi and assigning the request to Breweries_data
-url = “https://data.cityofchicago.org/resource/ijzp-q8t2.json?”
+url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json?"
 
+# defining the url function
+def request_call(url_value):
+    chicago_crime_data = requests.get(url_value)
+    chicago_crime_data_json = chicago_crime_data.json()
+
+    return chicago_crime_data_json
+
+
+
+# print(request_call(url))
