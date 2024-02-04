05c50a4327bbad9047bcbd3e112893be13dc4e36
“Ebibeni” <ebibeni.dbig@gmail.com>
2024-02-04 09:14:46 +0100

    Defining the data manipulation function

--- chicago_crime_dataset.py
+++ chicago_crime_dataset.py
@@ -43,81 +43,80 @@ len_api_data = len(api_data)
 
 # print(case_id)
 # print(case_number)
-data_List = []
-for length in api_data:
-    data_dic = {}
-    for key, value in length.items():
-        print(key,value)
-        if key == "id":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "case_number":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "date":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "block":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "iucr":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "primary_type":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "description":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "location_description":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "arrest":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "domestic":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "beat":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "district":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "ward":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "community_area":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "fbi_code":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "x_coordinate":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "y_coordinate":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "year":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "updated_on":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "latitude":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-        elif key == "longitude":
-                # print(f"{key}: {value}")
-            data_dic.update({key:value})
-    data_List.append(data_dic)
-        
+
+def data_manipulation():
+    data_List = []
+    for length in api_data:
+        data_dic = {}
+        for key, value in length.items():
+            print(key,value)
+            if key == "id":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "case_number":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "date":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "block":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "iucr":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "primary_type":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "description":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "location_description":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "arrest":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "domestic":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "beat":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "district":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "ward":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "community_area":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "fbi_code":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "x_coordinate":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "y_coordinate":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "year":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "updated_on":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "latitude":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+            elif key == "longitude":
+                    # print(f"{key}: {value}")
+                data_dic.update({key:value})
+        data_List.append(data_dic)
+    return data_List
 
 df = pd.DataFrame(api_data)
 # # print(df)
-# for i in df:
-#     for key, value in df.items():
-#         print(key,value)
 
 # df.to_csv(file_path, index=False)
